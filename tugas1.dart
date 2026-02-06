b5f9ee724ae66d13b428cf7d37321a8563c62658
reijjimuhammado2409 <muhammado2409@gmail.com>
2026-02-06 13:52:08 +0700

    tugas 1 dart

--- tugas1.dart
+++ tugas1.dart
@@ -1,24 +1,37 @@
 void main(){
-    String name = "Rezy"; // Benar
+    String name = "Rezy"; //
     int umur = 24;
     double tinggiBadan = 1.78;
-
+    bool statusaktif = true;
+    
+    // list untuk menyimpan 
     List<String> buku= ["Atomic Habits", "48 Power"];
-
-    Map<String, dynamic> dataPeserta = {
+    
+    //map untuk menyimpan nilai berpasangan
+    Map<String, dynamic> data = {
     "kelamin": "Pejantan Tangguh",
     "alamat": "Jl. Delta Serdang ",
     "profesi": "Peserta Mobile Programming",
-    "usia": 25,
+    "usia": 24,
     };
-
+    
+    ///fungsi ini untuk menampilkan biodata biodata 
     void biodata(){
-      print(name);
+      print("====================================");
+      print("nama : " + name);
       print(umur);
       print(tinggiBadan);
-      print(buku);
-      print(dataPeserta);
+      print("Apakah saya anak ppkd? ${statusaktif ? "Benar" : "Salah"}");
+      print(buku [0]);
+      print(buku [1]);
+      print("kelamin : ${data["kelamin"]}");
+      print("alamat : ${data["alamat"]}");
+      print("profesi : ${data["profesi"]}");
+      print("usia : ${data["usia"]}");
+      print("====================================");
     }
+    
+    /// memanggil data biodata
     biodata();
 }
 
